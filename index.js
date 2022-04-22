5879896d7990a8fe079b4b10f307f25d2f2b21b0
Ho Ngoc Quyen <ngocquyen.work@gmail.com>
2022-04-22 16:04:44 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Worlda!");
+  res.send("Hello Worlda!");
+  res.send("Hello Worlda!");
+  res.send("Hello Worlda!");
+  res.send("Hello Worlda!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
