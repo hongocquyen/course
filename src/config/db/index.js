c6c712cbdfbd728ef6dfa702fd104780cf876b57
Ho Ngoc Quyen <ngocquyen.work@gmail.com>
2022-06-04 00:16:24 +0700

    add mongodb to heroku

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/f8_education_dev");
+    await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/f8_education_dev");
     console.log("Connect DB successfully!!");
   } catch (error) {
     console.log("Connect DB failed!!");
